38b47ae13df67deca39fe480b185744999e7b0e2
TheintTheintSH <theinttheintsoehtet.edka@gmail.com>
2025-02-10 15:25:40 +0630

    todo board

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import AddTask from "./components/AddTask";
+import ToDo from "./components/ToDo";
 
 function App() {
   const [taskList, setTaskList] = useState([]);
@@ -7,19 +8,23 @@ function App() {
 
   return (
     <>
-      <h1 className="text-2xl font-bold py-4 pl-6">The Task Tracker</h1>
+      <h1 className="text-2xl font-bold py-6 pl-6">The Task Tracker</h1>
       <p className="text-xl pl-6">Hi there!</p>
       <div className="flex flex-row items-center">
         <p className="text-xl pl-6">Click</p>
         <AddTask taskList={taskList} setTaskList={setTaskList} />
         <p className="text-xl my-2">to add a new task</p>
       </div>
-      {taskList.map((task, i) => (
-        <>
-          <p>{task.projectName}</p>
-          <p>{task.taskDescription}</p>
-        </>
-      ))}
+      <div>
+        <h2 className="ml-6 text-xl font-semibold w-3/4 max-w-lg my-4 py-2 px-4 bg-gray-300">
+          To Do:
+        </h2>
+        {taskList.map((task, i) => (
+          <>
+            <ToDo key={i} task={task} />
+          </>
+        ))}
+      </div>
     </>
   );
 }
