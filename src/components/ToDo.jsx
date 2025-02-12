cdfd6a21ef44e3c33f08e606de85677405fc5cdb
TheintTheintSH <theinttheintsoehtet.edka@gmail.com>
2025-02-12 15:43:41 +0630

    adding drag

--- src/components/ToDo.jsx
+++ src/components/ToDo.jsx
@@ -1,9 +1,16 @@
 import { useEffect, useState } from "react";
 import EditTask from "./EditTask";
+import { useDrag } from "react-dnd";
 
 const ToDo = ({task, index, taskList, setTaskList}) => {
     const [time, setTime] = useState(task.duration);
     const [running, setRunning] = useState(false);
+    const [{ isDragging }, drag] = useDrag(() => ({
+        type: "todo",
+        collect: (monitor) => ({
+            isDragging: !!monitor.isDragging(),
+        })
+    }))
 
     useEffect(() => {
         let interval;
@@ -41,7 +48,7 @@ const ToDo = ({task, index, taskList, setTaskList}) => {
 
     return (
         <>
-            <div className="flex flex-col items-start justify-start bg-white my-4 ml-6 py-4 px-6 w-3/4 max-w-lg">
+            <div className="flex flex-col items-start justify-start bg-white my-4 ml-6 py-4 px-6 w-3/4 max-w-lg" ref={drag}>
                 <div className="w-full flex flex-row justify-between">
                     <p className="font-semibold text-xl">{task.projectName}</p>
                     <EditTask task={task} index={index} taskList={taskList} setTaskList={setTaskList} />
